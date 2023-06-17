c76573321c0f7ef934b42080e2b4d4a9e4341753
SergTsch87 <48403775+SergTsch87@users.noreply.github.com>
2023-06-17 12:49:59 +0300

    Update exchange.py: Add decorator timer(func)
    Decorator for measuring the execution time of a function

--- exchange.py
+++ exchange.py
@@ -3,3 +3,21 @@ import json
 import xmltodict
 import requests
 from urllib.request import urlopen
+
+
+# # # -------------------------------------------------------
+# Декоратор для заміру часу виконання функції
+
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Функція {func.__name__} виконалася за {execution_time} секунд")
+        return result
+    return wrapper
+
+# -------------------------------------------------------
